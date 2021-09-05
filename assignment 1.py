c652f7b9fa8b6b1b0689e3d178bd5a5aa4fc77b4
Jyoti-354 <89734710+Jyoti-354@users.noreply.github.com>
2021-09-05 23:54:38 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,24 @@
+# 1. add two number
+a=10
+b=20
+c=a+b
+print(c)
+
+# 3.print variable and strings in one line with the help of f string
+marks = 10
+name = 'Smith'
+age = 12
+print(f"The marks of {name} is {marks} and age is {age}")
+ 
+# 4. How to add space and new line in the print statement
+# for add space in the print statement 
+print("Hello \t Everyone")
+# for add new line in the print statement
+print("Hello \n Everyone")
+
+# 2.Try to add integer with string and see the output 
+a =10
+b ='Ten'
+c = a+b
+print(c)
+# we cannot add integer with string if we do we will get error
